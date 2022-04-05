0140cfce11ce56ef82477562602e7424e997e9dd
dandocmando <danferguson2002@gmail.com>
2022-04-05 14:50:47 +1000

    completed account investment calculations

--- src/Account.java
+++ src/Account.java
@@ -6,12 +6,16 @@ Description: Account Class, calculates the user investment
 SENG1110 Programming Assignment 1
  */
 
+import java.math.BigDecimal; // used to round vars to 2 dec places
+import java.math.RoundingMode;
+
 public class Account
 {
     double inv_rate;
-    int numberOfWeeks;
-    double amount;
+    int num_wks;
+    int n = 52;
     double inv;
+    double t = 1; // if we wanted to calculate investment over multiple years this would be used
  
    // constructor
     public Account()
@@ -19,11 +23,15 @@ public class Account
         
     }
     public double calcInv(){
-        double A;
-        //TA = inv*(1+(inv_rate/100)/numberOfWeeks)*Math.pow(51,1);
+        double FV; // value of investment, means Future Value
+        //TA = inv*(1+(inv_rate/100)/numberOfWeeks)*Math.pow(51,1); // previous attempts at creating algo
         //TA = Math.pow((1+inv_rate/12), 12)+(Math.pow((1+inv_rate/12), 12)-1)/(inv_rate/12);
-        A = ((Math.pow((1+(inv_rate/12)), 12)*inv-1) / (inv_rate/12)) + 5*Math.pow((1+(inv_rate/12)), 12);
-        return A;
+        //A = ((Math.pow((1+(inv_rate/12)), 12)*inv-1) / (inv_rate/12)) + 5*Math.pow((1+(inv_rate/12)), 12);
+        FV = inv*((Math.pow((1+inv_rate/n),(num_wks*t))-1)/(inv_rate/n))*(1+inv_rate/n);
+        // a very complicated ( yet far superior) way of finding the compound interest from
+        // regular contributions. This was the hardest part of the assignment to create.
+        // this formula compounds weekly and can calculate the value from 1 to 52 weeks.
+        return new BigDecimal(FV).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
     }
      
     public void setinvRate(double inputRate){ inv_rate = inputRate/100;}
@@ -35,8 +43,9 @@ public class Account
     public void setInv(double inputInvestment){ inv = inputInvestment;}
     public double getInv(){return inv;}
 
-    public void setNumberOfWeeks(int inputWeeks){numberOfWeeks = inputWeeks;}
-    public int getNumberOfWeeks(){return numberOfWeeks;}
+    public void setNum_wks(int inputWeeks){
+        num_wks = inputWeeks;}
+    public int getNum_wks(){return num_wks;}
 
     
 }
--- src/CalculatorInterface.java
+++ src/CalculatorInterface.java
@@ -73,24 +73,24 @@ public class CalculatorInterface
             }
         }
 
-        System.out.println("Enter the investment rate as a %:");
+        System.out.print("Enter the investment rate as a %: ");
         double inv_rate;
         inv_rate = console.nextDouble();
-        console.nextLine();
+        //console.nextLine();
         account.setinvRate(inv_rate);
 
-        System.out.println("Would you like to invest for a entire year?");
-        System.out.print("Enter Y or N:");
+        System.out.println("Would you like to invest for an entire year?");
+        System.out.print("Enter Y or N: ");
         String invest_year = console.next();
 
         if(Objects.equals(invest_year, "Y")){
-            account.setNumberOfWeeks(52);
+            account.setNum_wks(52); // assigns weeks to 52 as that is an entire year in wks
         }
         else{
             System.out.print("Enter the number of weeks you would like to invest for: ");
             int invest_weeks = console.nextInt();
-            console.nextLine();
-            account.setNumberOfWeeks(invest_weeks);
+            //console.nextLine();
+            account.setNum_wks(invest_weeks);
         }
         System.out.println(account.getinvRate());
         System.out.println(account.calcInv());
