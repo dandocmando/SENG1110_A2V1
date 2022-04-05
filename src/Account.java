04aefab5fe096ca860c444262ab6e3b918421181
dandocmando <danferguson2002@gmail.com>
2022-04-05 17:17:29 +1000

    pc sync

--- src/Account.java
+++ src/Account.java
@@ -6,36 +6,24 @@ Description: Account Class, calculates the user investment
 SENG1110 Programming Assignment 1
  */
 
-import java.math.BigDecimal; // used to round vars to 2 dec places
-import java.math.RoundingMode;
-
 public class Account
 {
-    private double inv_rate; // investment rate as a % (0.29)
-    private int num_wks; // number of weeks the investment stays invested
-    private double inv; // value of weekly contribution
-
+    double inv_rate;
+    int numberOfWeeks;
+    double amount;
+    double inv;
+ 
    // constructor
     public Account()
     {
         
     }
     public double calcInv(){
-        double FV; // value of investment, means Future Value
-        double t = 1; // if we wanted to calculate investment over multiple years this would be used
-        int n = 52; // number of weeks in a year, I have used simple var names to condense the FV line
-
-        //FA = inv*(1+(inv_rate/100)/numberOfWeeks)*Math.pow(51,1); // previous attempts at creating algo
-        //FA = Math.pow((1+inv_rate/12), 12)+(Math.pow((1+inv_rate/12), 12)-1)/(inv_rate/12);
-        //FA = ((Math.pow((1+(inv_rate/12)), 12)*inv-1) / (inv_rate/12)) + 5*Math.pow((1+(inv_rate/12)), 12);
-
-        FV = inv*((Math.pow((1+inv_rate/n),(num_wks*t))-1)/(inv_rate/n))*(1+inv_rate/n);
-        // a very complicated ( yet far superior) way of finding the compound interest from
-        // regular contributions. This was the hardest part of the assignment to create.
-        // this formula compounds weekly and can calculate the value from 1 to 52 weeks.
-        // also calculates assuming that the contribution is made at the start of each week.
-
-        return new BigDecimal(FV).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
+        double A;
+        //TA = inv*(1+(inv_rate/100)/numberOfWeeks)*Math.pow(51,1);
+        //TA = Math.pow((1+inv_rate/12), 12)+(Math.pow((1+inv_rate/12), 12)-1)/(inv_rate/12);
+        A = ((Math.pow((1+(inv_rate/12)), 12)*inv-1) / (inv_rate/12)) + 5*Math.pow((1+(inv_rate/12)), 12);
+        return A;
     }
      
     public void setinvRate(double inputRate){ inv_rate = inputRate/100;}
@@ -47,9 +35,8 @@ public class Account
     public void setInv(double inputInvestment){ inv = inputInvestment;}
     public double getInv(){return inv;}
 
-    public void setNum_wks(int inputWeeks){
-        num_wks = inputWeeks;}
-    public int getNum_wks(){return num_wks;}
+    public void setNumberOfWeeks(int inputWeeks){numberOfWeeks = inputWeeks;}
+    public int getNumberOfWeeks(){return numberOfWeeks;}
 
     
 }
